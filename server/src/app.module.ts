15a24ae553d94b261dceb4b269087b508e9cba2d
Kyrollos Bekhet <kirobekhet@gmail.com>
2021-09-25 23:10:03 +0000

    Broadcasting the message sent the message sent to the server.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+//import { AppController } from './app.controller';
+//import { AppService } from './app.service';
+import { ChatGateway } from './chat.gateway';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [ChatGateway],
 })
 export class AppModule {}
