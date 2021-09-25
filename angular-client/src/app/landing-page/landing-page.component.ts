60211e1f08b20f98fdb035a5f02109909e0fa012
Batikan Iscan <batikan.iscan@polymtl.ca>
2021-09-25 13:34:26 -0400

    refactor: Added verification infrastructure
    User is alerted of empty username via popup message

--- angular-client/src/app/landing-page/landing-page.component.ts
+++ angular-client/src/app/landing-page/landing-page.component.ts
@@ -14,10 +14,23 @@ export class LandingPageComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  submit(username: string) {
-    if (username) {
+  submit(username: string): void {
+    if(this.verifyUsername(username)) {
       console.log(username);
       this.username = username;
+      //TODO: add server request to login to server
+      //TODO: reset form
     }
+  }
+
+  verifyUsername(username: string): boolean {
+    const usernameEmpty: boolean = username.length <= 0;
+    const usernameExistsOnServer: boolean = false; //TODO: add server request via service
+    
+    if (usernameEmpty) {
+      alert('ERREUR! Veuillez entrer un nom d\'utilisateur!');
+    }
+    
+    return !usernameEmpty && !usernameExistsOnServer;
   }
 }
