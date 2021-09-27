8e60c29c6a6a97d3c4abd7e856492190d02cabf4
Batikan Iscan <35923632+batikaniscan@users.noreply.github.com>
2021-09-27 00:54:05 -0400

    refactor: Modified code to validate on submit

--- angular-client/src/app/landing-page/landing-page.component.ts
+++ angular-client/src/app/landing-page/landing-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-landing-page',
@@ -8,27 +8,44 @@ import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class LandingPageComponent implements OnInit {
   
+  username: string = '';
   usernameForm: FormGroup;
-  readonly DEFAULT_USERNAME: string = '';
+  usernameList: string[] = [];
+
+  //usernameListStub: string[] = [ 'abc123', 'paul', 'batikan-iscan' ];
 
   //Validators.minLength(1), Validators.maxLength(20), Validators.pattern('^[A-Za-z0-9_-]*$')
   constructor(private formBuilder: FormBuilder) {
+    this.usernameList = [ 'abc123', 'paul', 'batikan-iscan' ];
+        
     this.usernameForm = this.formBuilder.group({
-      username: formBuilder.control(this.DEFAULT_USERNAME, [ Validators.required ])
+      username: formBuilder.control('', [ Validators.required, this.usernameNotLoggedIn ])
     });
   }
 
   ngOnInit(): void {
   }
 
+  // reference https://www.youtube.com/watch?v=R1JWdvD0dv8
   // reference https://www.youtube.com/watch?v=9YuoQrvQ7R8
-  // adapted from https://loiane.com/2017/08/angular-reactive-forms-trigger-validation-on-submit/ 
+  // adapted from https://loiane.com/2017/08/angular-reactive-forms-trigger-validation-on-submit/
   public onSubmit():void {
-    if (this.usernameForm.valid) {
-      console.log(this.usernameForm.value['username']);
-      this.usernameForm.reset();
-    } else {
-      this.usernameForm.get(['username'])?.markAsTouched( {onlySelf: true});
+    let username = this.usernameForm.value['username'];
+    
+    console.log(username);
+    this.usernameForm.reset();
+    this.username = username; 
+  }
+
+  private usernameNotLoggedIn(control: AbstractControl): ValidationErrors | null {
+    if (control.value == null) {
+      return null;
     }
+
+    if ([ 'abc123', 'paul', 'batikan-iscan' ].indexOf(control.value) >= 0) {
+      return { 'usernameAlreadyExists' : true };
+    }
+
+    return null;
   }
 }
