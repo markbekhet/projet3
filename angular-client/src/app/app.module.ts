c36ab1fff62a3d8ed7c8fa8cfdabb1ac18f91d16
Batikan Iscan <35923632+batikaniscan@users.noreply.github.com>
2021-09-28 14:20:22 -0400

    feat: Added routing to the application
    Created communication-page component, routing module and specific paths to the landing page and login page

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -2,22 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { RequestService } from 'src/request.service';
+import { CommunicationPageComponent } from './communication-page/communication-page.component';
+
+const appRoutes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'user', component: CommunicationPageComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    CommunicationPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [
     RequestService
