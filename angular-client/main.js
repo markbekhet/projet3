8c8a99bc62318b119fe4398123a164fa83016a16
Batikan Iscan <35923632+batikaniscan@users.noreply.github.com>
2021-10-01 00:07:52 -0400

    style: Missing semi-colons

--- angular-client/main.js
+++ angular-client/main.js
@@ -3,17 +3,16 @@ const { app, BrowserWindow } = require('electron');
 function createWindow () {
     const window = new BrowserWindow({
       width: 800,
-      height: 600
-    })
+      height: 1000
+    });
   
-    window.loadFile(`${__dirname}/dist/index.html`)
-    //window.loadFile('C:/Users/batik/Documents/GitHub/log3900-101/angular-client/dist/angular-client/index.html');
+    window.loadFile(`${__dirname}/dist/index.html`);
   }
 
   app.whenReady().then(() => {
-    createWindow()
+    createWindow();
   })
 
   app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit()
+    if (process.platform !== 'darwin') app.quit();
   })
