0d556196e2a8e4573a20a896dee7f151bb76ff72
cora-maltese <paul.corrand@hotmail.fr>
2021-10-01 19:01:24 -0400

    refactor: Lots of refactoring and moving around files

--- angular-client/src/app/components/communication-page/communication-page.component.ts
+++ angular-client/src/app/components/communication-page/communication-page.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-import { RequestService } from 'src/request.service';
-import { ClientMessage, ServerMessage } from '../MessageMeta';
-import { ChatService } from '../chat.service';
+import { ChatService } from 'src/app/services/chat.service';
+import { RequestService } from 'src/app/services/request.service';
+import { Message } from 'src/app/models/MessageMeta';
 
 @Component({
   selector: 'app-communication-page',
@@ -14,13 +14,16 @@ import { ChatService } from '../chat.service';
 export class CommunicationPageComponent implements OnInit {
 
   username: string = '';
-  messages: ServerMessage[] = [];
-
+  messages: Message[] = [];
   messageForm: FormGroup;
 
-  constructor(private router: Router, private activeRoute: ActivatedRoute, 
-    private request: RequestService, private formBuilder: FormBuilder, 
-    private chat: ChatService) {
+  constructor(
+    private router: Router,
+    private activeRoute: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private chat: ChatService,
+    private request: RequestService
+  ) {
     this.messageForm = this.formBuilder.group({
       message: formBuilder.control('', [ Validators.required])
     });
@@ -28,11 +31,10 @@ export class CommunicationPageComponent implements OnInit {
 
   ngOnInit(): void {
     this.username = this.activeRoute.snapshot.params['username'];
-    this.chat.connect();
-
     this.messages = [];
+    // this.chat.connect();
 
-    this.chat.getNewMessage().subscribe((message: ServerMessage) => {
+    this.chat.getNewMessage().subscribe((message: Message) => {
       if (message.clientName) {
         this.messages.unshift(message);
       }
@@ -41,31 +43,10 @@ export class CommunicationPageComponent implements OnInit {
     })
   }
 
-  ngOnDestroy() {
-    this.chat.disconnect();
-    this.disconnect();
-  }
-
-  disconnect(): void {
-    //disconnect user
-    try {
-      this.request.disconnectClient(this.username)
-      .subscribe(
-        code => {
-          this.router.navigate(['/']);
-        }, //this.username = username
-        err => console.log(err)); //LandingPageComponent.usernameExists = true
-    } catch (e: any) { 
-
-    }
-    this.messages = []; 
-    
-  }
-
   onSubmit() {
-    let currentDate: Date = new Date();
-    
-    let message: ServerMessage = {
+    const currentDate: Date = new Date();
+
+    const message: Message = {
       clientName: this.username,
       message: this.messageForm.value['message'],
       date: {
@@ -79,12 +60,33 @@ export class CommunicationPageComponent implements OnInit {
 
     this.chat.sendMessage(message);
     console.log('client sent: ' + message);
-    
+
     this.messageForm.reset();
   }
 
-  public self(clientName: string):boolean {
-    return this.username === clientName; 
+  public self(clientName: string): boolean {
+    return this.username === clientName;
   }
 
+  ngOnDestroy() {
+    this.messages = [];
+    this.chat.disconnect();
+    this.disconnect();
+  }
+
+  disconnect(): void {
+    //disconnect user
+    try {
+      this.request.disconnectClient(this.username)
+      .subscribe(
+        code => {
+          this.router.navigate(['/']);
+        }, //this.username = username
+        err => console.log(err)); //LandingPageComponent.usernameExists = true
+    } catch (e: any) {
+
+    }
+    this.messages = [];
+
+  }
 }
--- angular-client/src/app/services/chat.service.ts
+++ angular-client/src/app/services/chat.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { ClientMessage, ServerMessage } from './MessageMeta';
-
 import { BehaviorSubject, Observable } from 'rxjs';
 import { io } from "socket.io-client";
+
+import { Message } from '../models/MessageMeta';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +11,9 @@ export class ChatService {
   PATH = 'http://projet3-101.eastus.cloudapp.azure.com:3000/';
   //PATH = 'localhost:3000';
 
-  public message$: BehaviorSubject<ServerMessage> = new BehaviorSubject<ServerMessage>({
+  socket = io(this.PATH);
+
+  public message$: BehaviorSubject<Message> = new BehaviorSubject<Message>({
     clientName: '',
     message: '',
     date: {
@@ -20,15 +22,14 @@ export class ChatService {
       seconds: ''
     }
   });
-  socket = io(this.PATH);
-  
-  public sendMessage(message: ServerMessage) {
+
+  public sendMessage(message: Message) {
     console.log('chat service sent: ' + message.message);
     this.socket.emit('msgToServer', JSON.stringify(message));
   }
 
   public getNewMessage = () => {
-    this.socket.on('msgToClient', (message: ServerMessage) => {
+    this.socket.on('msgToClient', (message: Message) => {
       console.log('chat service received: ' + message.message);
       this.message$.next(message);
     });
