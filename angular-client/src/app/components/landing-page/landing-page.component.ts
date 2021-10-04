e30c930f6e69ca273a9aa9af446377f986a60f5c
Paul Corrand <paul.corrand@hotmail.fr>
2021-10-04 16:15:12 +0000

    config&refactor/MovedAroundFilesAndEnabledEsLint

--- angular-client/src/app/components/landing-page/landing-page.component.ts
+++ angular-client/src/app/components/landing-page/landing-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { RequestService } from 'src/request.service';
+import { RequestService } from 'src/app/services/request.service';
 
 @Component({
   selector: 'app-landing-page',
@@ -9,41 +9,44 @@ import { RequestService } from 'src/request.service';
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent implements OnInit {
-  
+
   username: string = '';
   usernameForm: FormGroup;
-  static usernameExists: boolean = false;
+  usernameExists: boolean = false;
 
   //Validators.minLength(1), Validators.maxLength(20), Validators.pattern('^[A-Za-z0-9_-]*$')
-  constructor(private formBuilder: FormBuilder, private router: Router, private request: RequestService) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private request: RequestService
+  ) {
     this.usernameForm = this.formBuilder.group({
       username: formBuilder.control('', [ Validators.required])
     });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   // reference https://www.youtube.com/watch?v=R1JWdvD0dv8
   // reference https://www.youtube.com/watch?v=9YuoQrvQ7R8
   // adapted from https://loiane.com/2017/08/angular-reactive-forms-trigger-validation-on-submit/
   public async onSubmit() {
-    let username = this.usernameForm.value['username'];
-    
+    this.username = this.usernameForm.value['username'];
+
     try {
-      this.request.connectClient(username)
+      this.request.connectClient(this.username)
       .subscribe(
-        code => this.router.navigate(['/' + username]), //this.username = username
-        err => LandingPageComponent.usernameExists = true); //LandingPageComponent.usernameExists = true
-    
-      LandingPageComponent.usernameExists = false;
-    } catch (e: any) { } 
+        accepted => {
+          this.usernameExists = false;
+          this.router.navigate(['/' + this.username]); //this.username = username
+        },
+        forbidden => this.usernameExists = true
+      );
+
+      // this.usernameExists = false;
+    } catch (e: any) { }
       finally {
         this.usernameForm.reset();
-      } 
-    }
-
-  get staticUsernameExists() {
-    return LandingPageComponent.usernameExists;
+      }
   }
 }
