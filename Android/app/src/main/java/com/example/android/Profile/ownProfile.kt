29e0e5ff0abb03ff65b64ffe19e49dac3e9b9471
markbekhet <markonsy72@gmail.com>
2021-10-06 21:21:24 -0400

    Build the UI of the page having the general user information

---
+++ Android/app/src/main/java/com/example/android/Profile/ownProfile.kt
@@ -0,0 +1,12 @@
+package com.example.android.Profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android.R
+
+class ownProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_own_profile)
+    }
+}
