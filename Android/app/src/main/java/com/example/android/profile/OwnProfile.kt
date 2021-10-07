d0be3ca566442a23b8a16d50c7c9ce2a1e149728
markbekhet <markonsy72@gmail.com>
2021-10-07 10:09:30 -0400

    Create a dialog pop up to modify the paramaters

--- Android/app/src/main/java/com/example/android/profile/OwnProfile.kt
+++ Android/app/src/main/java/com/example/android/profile/OwnProfile.kt
@@ -1,12 +1,34 @@
 package com.example.android.profile
 
+import android.app.Dialog
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.android.R
 
 class OwnProfile : AppCompatActivity() {
+    var modifyParamsDialog : Dialog? =null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_own_profile)
+        var modifyParams: Button = findViewById(R.id.modifyParams)
+
+        modifyParams.setOnClickListener{
+            modifyParamsDialog = ModifyParams(this)
+            modifyParamsDialog!!.create()
+            modifyParamsDialog!!.show()
+        }
+    }
+}
+
+class ModifyParams(context: Context) : Dialog(context){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.popup_modify_parameters)
+        var okButton:Button = findViewById(R.id.popup)
+        okButton.setOnClickListener {
+            super.hide()
+        }
     }
 }
