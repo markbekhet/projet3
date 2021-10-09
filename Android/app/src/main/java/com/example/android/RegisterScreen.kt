976130b756513525826e76431d6553ddd9b5e3ba
markbekhet <markonsy72@gmail.com>
2021-10-09 19:08:58 -0400

    Add lastName field toregister form

--- Android/app/src/main/java/com/example/android/RegisterScreen.kt
+++ Android/app/src/main/java/com/example/android/RegisterScreen.kt
@@ -3,12 +3,15 @@ package com.example.android
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android.client.ClientService
 import kotlinx.android.synthetic.main.activity_register_screen.*
 
 class RegisterScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_screen)
+
+        var clientService = ClientService()
 
         login.setOnClickListener(){
             startActivity(Intent(this, LoginScreen::class.java))
--- Android/app/src/main/java/com/example/android/profile/OwnProfile.kt
+++ Android/app/src/main/java/com/example/android/profile/OwnProfile.kt
@@ -6,10 +6,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import androidx.core.widget.doAfterTextChanged
 import com.example.android.R
 import com.example.android.client.ClientInfo
 import com.example.android.client.ClientService
+import kotlinx.android.synthetic.main.popup_modify_parameters.*
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -82,16 +85,34 @@ class ModifyParams(context: Context) : Dialog(context){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.popup_modify_parameters)
-        var okButton:Button = findViewById(R.id.confirm)
+        val okButton:Button = findViewById(R.id.confirm)
+        val cancelButton: Button = findViewById(R.id.cancel)
+        val newPassword: EditText = findViewById(R.id.newPassword)
+        val confirmNewPassword: EditText = findViewById(R.id.confirmNewPassword)
+        val newNickname: EditText = findViewById(R.id.newNickname)
+
+        newPassword.doAfterTextChanged {
+            if(confirmNewPassword.text.isEmpty()){
+
+            }else if(oldPassword.text.isEmpty()){
+
+            }
+        }
+
         okButton.setOnClickListener {
             //The request to update the information will be sent before hiding the pop up
             super.dismiss()
         }
-        val cancelButton: Button = findViewById(R.id.cancel)
+
         cancelButton.setOnClickListener {
             super.dismiss()
         }
 
         setCanceledOnTouchOutside(true)
+    }
+
+    fun isPasswordEqual(newPass: String, confirmPass: String): Boolean {
+        return newPass == confirmPass
+
     }
 }
