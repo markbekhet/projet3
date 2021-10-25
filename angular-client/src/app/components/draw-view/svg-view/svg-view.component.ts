4a39f0aaf14c6baebd4369db7b0b980c163e270b
Batikan <batikan.iscan@polymtl.ca>
2021-10-25 15:34:26 -0400

    feat: Ellipse is finished

--- angular-client/src/app/components/draw-view/svg-view/svg-view.component.ts
+++ angular-client/src/app/components/draw-view/svg-view/svg-view.component.ts
@@ -4,17 +4,28 @@ import { ChoosenColors } from 'src/app/models/chosen-colors';
 import { DrawingContent, DrawingStatus } from 'src/app/models/drawing-content';
 import { CanvasBuilderService } from 'src/app/services/canvas-builder/canvas-builder.service';
 import { ColorPickingService } from 'src/app/services/colorPicker/color-picking.service';
+import { Ellipse } from 'src/app/services/draw-tool/ellipse';
 import { InputObserver } from 'src/app/services/draw-tool/input-observer';
 import { Pencil } from 'src/app/services/draw-tool/pencil';
 import { Rectangle } from 'src/app/services/draw-tool/rectangle';
 import { InteractionService } from 'src/app/services/interaction-service/interaction.service';
 import { MouseHandler } from 'src/app/services/mouse-handler/mouse.handler';
 
+//Crayon
 const POINTS_REGEX= new RegExp(`points="([0-9.?]+ [0-9.?]+(,[0-9.?]+ [0-9.?]+)*)`);
+
+//Rect
 const X_REGEX = new RegExp(`x="([-?0-9.?]*)"`);
 const Y_REGEX = new RegExp(`y="([-?0-9.?]*)"`);
 const WIDTH_REGEX = new RegExp(`width="([-?0-9.?]*)"`);
 const HEIGHT_REGEX = new RegExp(`height="([-?0-9.?]*)"`);
+
+//Ellipse
+const CX_REGEX = new RegExp(`cx="([-?0-9.?]*)"`);
+const CY_REGEX = new RegExp(`cy="([-?0-9.?]*)"`);
+const RX_REGEX = new RegExp(`rx="([-?0-9.?]*)"`);
+const RY_REGEX = new RegExp(`ry="([-?0-9.?]*)"`);
+
 const TRANSLATE_REGX = new RegExp(`transform="translate\(([-?0-9.?])+,([-?0-9.?])+\)"`);
 
 @Component({
@@ -127,10 +138,12 @@ export class SvgViewComponent implements OnInit, AfterViewInit {
   // A map is used instead of if/else
   createTools(){
     const pencil = new Pencil(false, this.interactionService);
-    const rectangle = new Rectangle(true, this.interactionService);
+    const rectangle = new Rectangle(false, this.interactionService);
+    const ellipse = new Ellipse(true, this.interactionService);
 
     this.toolsContainer.set('Crayon', pencil);
     this.toolsContainer.set('Rectangle', rectangle);
+    this.toolsContainer.set('Ellipse', ellipse);
   }
 
   // This method will be modified especially with the introduction of selected status and deleted status
@@ -140,13 +153,13 @@ export class SvgViewComponent implements OnInit, AfterViewInit {
       if(!this.contents.has(data.contentId)){
         //new elements
         let newObj!: SVGElement;
-        console.log(data.contentId);
         if (data.drawing.includes('polyline')) {
           newObj = this.createSVGPolyline(data.drawing);
         } else if (data.drawing.includes('rect')) {
           newObj = this.createSVGRect(data.drawing);
         } else if (data.drawing.includes('ellipse')) {
-
+          newObj = this.createSVGEllipse(data.drawing);
+          //console.log(data.drawing);
         }
         
         if (newObj !== null) {
@@ -159,12 +172,13 @@ export class SvgViewComponent implements OnInit, AfterViewInit {
         if(element !== undefined){
           //this.renderer.removeChild(this.inProgress.nativeElement,element);
           if (data.drawing.includes('polyline')) {
-          this.modifyPolyline(data.drawing, element);
+            this.modifyPolyline(data.drawing, element);
           } else if (data.drawing.includes('rect')) {
-          this.modifyRect(data.drawing, element);
+            this.modifyRect(data.drawing, element);
           } else if (data.drawing.includes('ellipse')) {
-  
-          }
+            this.modifyEllipse(data.drawing, element);
+            //console.log(data.drawing);
+         }
           this.renderer.appendChild(this.inProgress.nativeElement, element);
         }
       }
@@ -178,7 +192,7 @@ export class SvgViewComponent implements OnInit, AfterViewInit {
         } else if (data.drawing.includes('rect')) {
           this.modifyRect(data.drawing, element);
         } else if (data.drawing.includes('ellipse')) {
-
+          this.modifyEllipse(data.drawing, element);
         }
         this.renderer.appendChild(this.doneDrawing.nativeElement, element);
       }
@@ -212,6 +226,7 @@ export class SvgViewComponent implements OnInit, AfterViewInit {
 
   createSVGRect(drawing: string) {
     let element = this.renderer.createElement('rect', 'svg') as SVGRectElement;
+    console.log(element)
     let x = X_REGEX.exec(drawing);
     let y = Y_REGEX.exec(drawing);
     let width = WIDTH_REGEX.exec(drawing);
@@ -224,7 +239,6 @@ export class SvgViewComponent implements OnInit, AfterViewInit {
       this.renderer.setAttribute(element,'stroke', 'black');
       this.renderer.setAttribute(element, 'stroke-width','5');
       this.renderer.setAttribute(element, 'fill', 'none');
-    
     }
     return element;
   }
@@ -240,11 +254,46 @@ export class SvgViewComponent implements OnInit, AfterViewInit {
       this.renderer.setAttribute(element, 'y', y[1].toString());
       this.renderer.setAttribute(element, 'width', width[1].toString());
       this.renderer.setAttribute(element, 'height', height[1].toString());
-      
     }
   }
 
+  createSVGEllipse(drawing: string) {
+    console.log(drawing);
+    let element = this.renderer.createElement('ellipse', 'svg');
+    let cx = CX_REGEX.exec(drawing);
+    console.log(cx);
+    let cy = CY_REGEX.exec(drawing);
+    console.log(cy);
+    let rx = RX_REGEX.exec(drawing);
+    console.log(rx);
+    let ry = RY_REGEX.exec(drawing);
+    //console.log(ry);
+    if(cx !== null && cy !== null && rx !== null && ry !== null){
+      this.renderer.setAttribute(element, 'cx', cx[1].toString());
+      this.renderer.setAttribute(element, 'cy', cy[1].toString());
+      this.renderer.setAttribute(element, 'rx', rx[1].toString());
+      this.renderer.setAttribute(element, 'ry', ry[1].toString());
+      this.renderer.setAttribute(element,'stroke', 'black');
+      this.renderer.setAttribute(element, 'stroke-width','5');
+      this.renderer.setAttribute(element, 'fill', 'none');
+    }
+    console.log(element)
+    return element;
+  }
 
-
-
+  modifyEllipse(drawing: string, element: SVGElement){
+    let cx = CX_REGEX.exec(drawing);
+    let cy = CY_REGEX.exec(drawing);
+    let rx = RX_REGEX.exec(drawing);
+    let ry = RY_REGEX.exec(drawing);
+    if(cx !== null && cy !== null && rx !== null && ry !== null){
+      this.renderer.setAttribute(element, 'cx', cx[1].toString());
+      this.renderer.setAttribute(element, 'cy', cy[1].toString());
+      this.renderer.setAttribute(element, 'rx', rx[1].toString());
+      this.renderer.setAttribute(element, 'ry', ry[1].toString());
+      this.renderer.setAttribute(element,'stroke', 'black');
+      this.renderer.setAttribute(element, 'stroke-width','5');
+      this.renderer.setAttribute(element, 'fill', 'none');
+    }
+  }
 }
---
+++ angular-client/src/app/services/draw-tool/ellipse.ts
@@ -0,0 +1,49 @@
+import { DrawingStatus } from "src/app/models/drawing-content";
+import { InteractionService } from "../interaction-service/interaction.service";
+import { DrawingTool } from "./drawing-tool";
+import { Point } from "./point";
+import { Shape } from "./shape";
+
+const DEFAULT_LINE_THICKNESS = 5;
+export class Ellipse extends Shape {
+    
+    constructor(selected: boolean, interactionService: InteractionService){
+        super(selected, interactionService);
+        //this.updateColors();
+        this.updateAttributes();
+    }
+    updateAttributes(){
+        
+    }
+
+    setDimensions(p: Point[]): void {
+        this.startX = this.width > 0 ? p[0].x : p[p.length - 1].x;
+        this.startY = this.height > 0 ? p[0].y : p[p.length - 1].y;        
+    
+        super.setDimensions(p);
+        // Rectangle
+    }
+
+    // this is the function used to write the string
+    createPath(p:Point[]): string{
+        this.svgString = '';
+
+        this.setDimensions(p);
+        this.svgString += `<ellipse cx="${this.startX + Math.abs(this.width / 2)}"`;
+        this.svgString += ` cy="${this.startY + Math.abs(this.height / 2)}"`;
+        this.svgString += ` rx="${Math.abs(this.width / 2)}"`;
+        this.svgString += ` ry="${Math.abs(this.height / 2)}"`;
+
+        this.setAttributesToPath();
+
+        // end the divider
+        //this.svgString += '/>';
+
+        // can't have rectangle with 0 width or height
+        if (this.width === 0 || this.height === 0) {
+            //this.svgString = '';
+        }
+
+        return this.svgString;
+    }
+}
