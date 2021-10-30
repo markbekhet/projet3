fa8ef612068ca6fff14011f28fda6a6b6310a2a7
cora-maltese <paul.corrand@hotmail.fr>
2021-10-30 13:41:06 -0400

    chore: Added personal postgre local DB, installed server dependencies

--- server/src/config/typeorm.config.ts
+++ server/src/config/typeorm.config.ts
@@ -1,15 +1,27 @@
-import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
+// Kyro DB
+// export const typeormConfig: TypeOrmModuleOptions = {
+//   type: 'postgres',
+//   host: 'localhost',
+//   port: 5432,
+//   username: 'test_user',
+//   password: 'GetThisHands98',
+//   database: 'test_db',
+//   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+//   synchronize: true,
+//   uuidExtension: 'pgcrypto',
+// };
+
+// Paul DB
 export const typeormConfig: TypeOrmModuleOptions = {
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'test_user',
-    password: 'GetThisHands98',
-    database: 'test_db',
-    entities: [
-        __dirname + '/../**/*.entity{.ts,.js}',
-    ],
-    synchronize: true,
-    uuidExtension: 'pgcrypto'
-}
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'admin',
+  password: 'password',
+  database: 'colorimage',
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  synchronize: true,
+  uuidExtension: 'pgcrypto',
+};
