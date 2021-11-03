a364a9cb4e59f50eaf6d13921c52524c55ad0f26
adlain <adlain-ryan.batata@gmail.com>
2021-11-02 21:59:01 -0400

    ajout debut gallerie

---
+++ Android/app/src/main/java/com/example/android/client/Gallery.kt
@@ -0,0 +1,12 @@
+package com.example.android.client
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android.R
+
+class Gallery : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gallery)
+    }
+}
