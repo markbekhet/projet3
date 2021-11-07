046df57566a1a294d1a3c87d0a9b32b7b55f673b
cora-maltese <paul.corrand@hotmail.fr>
2021-11-07 17:06:45 -0500

    chore: Trying to add typeorm.config.dev.ts to gitignore but it won't work. So I recommend we comment and uncomment our configs.

--- server/src/config/typeorm.config.dev.ts
+++ server/src/config/typeorm.config.dev.ts
@@ -1,5 +1,19 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
+// Kyro DB
+// export const typeormConfigDevDB: TypeOrmModuleOptions = {
+//   type: 'postgres',
+//   host: 'localhost',
+//   port: 5432,
+//   username: 'test_user',
+//   password: 'GetThisHands98',
+//   database: 'test_db',
+//   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+//   synchronize: true,
+//   uuidExtension: 'pgcrypto',
+// };
+
+// Paul DB
 export const typeormConfigDevDB: TypeOrmModuleOptions = {
   type: 'postgres',
   host: 'localhost',
