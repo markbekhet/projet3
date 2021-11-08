e719556d7bb6fec528bf740dd0647dc44c99603d
markbekhet <markonsy72@gmail.com>
2021-11-08 13:43:03 -0500

    Join a drawing from the gallery

--- Android/app/src/main/java/com/example/android/client/Gallery.kt
+++ Android/app/src/main/java/com/example/android/client/Gallery.kt
@@ -1,13 +1,17 @@
 package com.example.android.client
 
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.android.Drawing
 import com.example.android.R
-import com.example.android.canvas.ContentDrawingSocket
-import com.example.android.canvas.GalleryDrawing
-import com.example.android.canvas.ReceiveDrawingInformation
+import com.example.android.SocketHandler
+import com.example.android.canvas.*
 import com.example.android.chat.ServerMessage
 import com.example.android.chat.UserMessage
 import com.xwray.groupie.GroupAdapter
@@ -15,6 +19,7 @@ import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
 import kotlinx.android.synthetic.main.activity_gallery.*
 import kotlinx.android.synthetic.main.activity_login_screen.*
+import kotlinx.android.synthetic.main.dessin.*
 import kotlinx.android.synthetic.main.draw.view.*
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
@@ -34,6 +39,8 @@ class Gallery : AppCompatActivity() {
         val linearLayoutManager: LinearLayoutManager = LinearLayoutManager(this)
         linearLayoutManager.orientation = LinearLayoutManager.VERTICAL
         displayDrawingGallery?.layoutManager = linearLayoutManager
+        SocketHandler.setChatSocket()
+        SocketHandler.establishChatSocketConnection()
         var gallery  = GalleryDrawing()
         var response: Response<ResponseBody>?=null
         runBlocking {
@@ -53,14 +60,18 @@ class Gallery : AppCompatActivity() {
     fun buildGallery(drawingList :ArrayList<ReceiveDrawingInformation>){
         galleryDisplay = GroupAdapter<GroupieViewHolder>()
         for(drawing in drawingList){
-            val galleryDrawing = GalleryItem()
+            val galleryDrawing = GalleryItem(this)
             galleryDrawing.set(drawing)
             galleryDisplay!!.add(galleryDrawing)
         }
         displayDrawingGallery!!.adapter = galleryDisplay
     }
+
+    fun startDrawingActivity(){
+        startActivity(Intent(this, Drawing::class.java))
+    }
 }
-class GalleryItem : Item<GroupieViewHolder>() {
+class GalleryItem(var context: Gallery) : Item<GroupieViewHolder>() {
     private var information: ReceiveDrawingInformation?= null
 
     override fun getLayout(): Int {
@@ -70,6 +81,32 @@ class GalleryItem : Item<GroupieViewHolder>() {
     override fun bind(viewHolder: GroupieViewHolder, position: Int) {
         println(information!!.name)
         viewHolder.itemView.name.text = information!!.name
+        val canvas = GalleryCanvasView(information!!.id!!, context)
+        canvas.parseExistingDrawings(information!!.contents)
+        val params: ViewGroup.LayoutParams = viewHolder.itemView.fl_drawing_view_gallery.getLayoutParams()
+        params.width= 300
+        params.height= 300
+        viewHolder.itemView.fl_drawing_view_gallery.setLayoutParams(params)
+        canvas.setBackgroundColor(
+            Color.parseColor("#ff${information!!.bgColor}"))
+        viewHolder.itemView.fl_drawing_view_gallery.addView(canvas)
+        viewHolder.itemView.fl_drawing_view_gallery.setOnClickListener {
+            DrawingUtils.currentDrawingId = information!!.id!!
+            var joinRequest = JoinDrawingDto(DrawingUtils.currentDrawingId,
+                ClientInfo.userId)
+
+            SocketHandler.getChatSocket()!!.emit("joinDrawing", joinRequest.toJson())
+            var i = 0
+            SocketHandler.getChatSocket()!!.on("drawingInformations"){ args ->
+                if(args[0]!=null && i==0){
+                    val data = args[0] as String
+                    DrawingUtils.drawingInformation =
+                        information
+                    context.startDrawingActivity()
+                    i++
+                }
+            }
+        }
 
     }
 
