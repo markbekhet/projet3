0b8128c50cfbdc8667e34e86c3333e02daf3d3e8
Kyrollos Bekhet <kirobekhet@gmail.com>
2021-11-08 08:15:19 -0500

    change in typeorm config file to be able to connect to the database

--- server/src/config/typeorm.config.dev.ts
+++ server/src/config/typeorm.config.dev.ts
@@ -1,27 +1,27 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 // Kyro DB
-// export const typeormConfigDevDB: TypeOrmModuleOptions = {
-//   type: 'postgres',
-//   host: 'localhost',
-//   port: 5432,
-//   username: 'test_user',
-//   password: 'GetThisHands98',
-//   database: 'test_db',
-//   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-//   synchronize: true,
-//   uuidExtension: 'pgcrypto',
-// };
+ export const typeormConfigDevDB: TypeOrmModuleOptions = {
+   type: 'postgres',
+   host: 'localhost',
+   port: 5432,
+   username: 'test_user',
+   password: 'GetThisHands98',
+   database: 'test_db',
+   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+   synchronize: true,
+   uuidExtension: 'pgcrypto',
+  };
 
 // Paul DB
-export const typeormConfigDevDB: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'admin',
-  password: 'password',
-  database: 'colorimage',
-  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  synchronize: true,
-  uuidExtension: 'pgcrypto',
-};
+//export const typeormConfigDevDB: TypeOrmModuleOptions = {
+//  type: 'postgres',
+//  host: 'localhost',
+//  port: 5432,
+//  username: 'admin',
+//  password: 'password',
+//  database: 'colorimage',
+//  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+//  synchronize: true,
+//  uuidExtension: 'pgcrypto',
+//};
