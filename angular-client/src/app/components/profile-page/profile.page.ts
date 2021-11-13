0507cbaf0e3df069b18e4ebe0de6c6d7bf09ca86
Batikan Iscan <35923632+batikaniscan@users.noreply.github.com>
2021-11-13 17:05:46 -0500

    feat: Profile page has functional update username form
    error handling to be done

--- angular-client/src/app/components/profile-page/profile.page.ts
+++ angular-client/src/app/components/profile-page/profile.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Status, User } from '@src/app/models/UserMeta';
 import { AuthService } from '@src/app/services/authentication/auth.service';
@@ -36,14 +37,14 @@ export class ProfilePage implements OnInit {
     drawingEditionHistories: [],
   };
 
+  updatePseudo: FormGroup;
+
   constructor(
     private router: Router,
     private auth: AuthService,
-    private socketService: SocketService
-  ) {}
-
-  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
-  ngOnInit(): void {
+    private socketService: SocketService,
+    private formBuilder: FormBuilder
+  ) {
     this.socketService.getUserProfile({
       userId: this.auth.token$.value,
       visitedId: this.auth.token$.value,
@@ -53,6 +54,33 @@ export class ProfilePage implements OnInit {
       this.user = profile;
       console.log(`user loaded : ${profile.pseudo}`);
     });
+
+    this.updatePseudo = this.formBuilder.group({
+      pseudo: formBuilder.control(this.user.pseudo, []),
+    });
+  }
+
+  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
+  ngOnInit(): void {}
+
+  async onSubmit(formPseudo: FormGroup) {
+    alert("we're ready to submit any changes");
+    // detect changes
+
+    // what to do
+    // if empty -> use old username
+    // if new username = old username -> use old username
+    // if new username = illegal (someone already has it) -> use old username + throw error message
+    // else use new username
+    const nouveauPseudo = this.verifyPseudo(formPseudo.controls.pseudo.value);
+    console.log(nouveauPseudo);
+  }
+
+  private verifyPseudo(nouveauPseudo: string) {
+    const CURRENT_PSEUDO: string = this.user.pseudo;
+    if (nouveauPseudo === '') return CURRENT_PSEUDO;
+    if (nouveauPseudo === CURRENT_PSEUDO) return CURRENT_PSEUDO;
+    return nouveauPseudo;
   }
 
   goLaunchingPage() {
