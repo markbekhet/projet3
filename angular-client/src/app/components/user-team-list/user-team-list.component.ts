c4894266609ce6b460055323861cdb495fb20892
Kyrollos <kirobekhet@gmail.com>
2021-11-15 21:37:41 -0500

    receive user updates from server

--- angular-client/src/app/components/user-team-list/user-team-list.component.ts
+++ angular-client/src/app/components/user-team-list/user-team-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Team } from '@src/app/models/teamsMeta';
 import { User } from '@src/app/models/UserMeta';
+//import { InteractionService } from '@src/app/services/interaction/interaction.service';
 import { SocketService } from '@src/app/services/socket/socket.service';
 
 @Component({
@@ -8,13 +9,14 @@ import { SocketService } from '@src/app/services/socket/socket.service';
   templateUrl: './user-team-list.component.html',
   styleUrls: ['./user-team-list.component.scss']
 })
-export class UserTeamListComponent implements OnInit {
+export class UserTeamListComponent implements OnInit, AfterViewInit {
 
   userList: User[];
   teamList: Team[];
-  constructor(private socketService: SocketService) { 
+  constructor(private socketService: SocketService/*, private interactionService: InteractionService*/) { 
     this.userList = []
     this.teamList = []
+    
   }
 
   ngOnInit(): void {
@@ -31,6 +33,22 @@ export class UserTeamListComponent implements OnInit {
     })
     console.log(this.userList);
     console.log(this.teamList);
+    
+  }
+  ngAfterViewInit(){
+    this.socketService.socket!.on("userUpdate", (data: any)=>{
+      let dataMod: User = JSON.parse(data);
+      let found = false;
+      this.userList.forEach((user)=>{
+        if(user.id === dataMod.id){
+          user = dataMod;
+          found = true;
+        }
+      })
+      if(!found){
+        this.userList.push(dataMod);
+      }
+    })
   }
 
 }
