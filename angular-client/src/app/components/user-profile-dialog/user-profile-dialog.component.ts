a99c32a9aa03a0c9431372a684ba07dfcc36cdc3
Batikan Iscan <35923632+batikaniscan@users.noreply.github.com>
2021-11-25 08:50:45 -0500

    feat: Users can see other online users' avatars on their user profiles

--- angular-client/src/app/components/user-profile-dialog/user-profile-dialog.component.ts
+++ angular-client/src/app/components/user-profile-dialog/user-profile-dialog.component.ts
@@ -1,8 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-//import { ModalWindowService } from '@services/window-handler/modal-window.service';
 import { Status, User } from '@src/app/models/UserMeta';
 import { AuthService } from '@src/app/services/authentication/auth.service';
+import { AvatarService } from '@src/app/services/avatar/avatar.service';
 import { SocketService } from '@src/app/services/socket/socket.service';
 
 @Component({
@@ -14,13 +14,14 @@ export class UserProfileDialogComponent implements OnInit {
   visitedUser: User = {
     pseudo: '',
     status: Status.ONLINE,
+    avatar: '',
   };
 
 
   constructor(
     private auth: AuthService,
     private socketService: SocketService,
-    //private windowService: ModalWindowService,
+    private avatarService: AvatarService,
     @Inject(MAT_DIALOG_DATA) public data: string
     ) {
       const visitedUserData = data as User;
@@ -36,6 +37,11 @@ export class UserProfileDialogComponent implements OnInit {
   ngOnInit(): void {
     this.socketService.receiveUserProfile().subscribe((profile: User) => {
       this.visitedUser = profile;
+      console.log(this.visitedUser.avatar);
     });
+  }
+
+  decodeAvatar() {
+    return this.avatarService.decodeAvatar(this.visitedUser.avatar!);
   }
 }
