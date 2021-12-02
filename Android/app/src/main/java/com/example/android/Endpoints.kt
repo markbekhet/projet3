87d6f4598b810ada07c58c0b5276b253eb19ae03
markbekhet <markonsy72@gmail.com>
2021-12-02 13:27:10 -0500

    Adjust drawings wdth in the gallery and set a default width for the gallery

--- Android/app/src/main/java/com/example/android/Endpoints.kt
+++ Android/app/src/main/java/com/example/android/Endpoints.kt
@@ -8,8 +8,8 @@ const val disconnect = "/disconnect"
 const val delete = "/delete"
 
 const val localUrl = "http://10.200.41.34:3000"
-const val productionUrl = "http://projet3-101.eastus.cloud.azure.com:3000"
-const val url = localUrl
+const val productionUrl = "http://projet3-101.eastus.cloudapp.azure.com:3000"
+const val url = productionUrl
 
 const val drawingNamespace = "/drawing"
 const val chatNamespace = "/chat"
