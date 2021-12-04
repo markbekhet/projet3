034d2f6b6238cde48f7239ca51d832a377d3cbc7
markbekhet <markonsy72@gmail.com>
2021-12-04 18:17:26 -0500

    Remove the intent and the action associated to the notification
    Before this change, the notification had a redirection to the landing
    page. This can have some problems because we cannot leave the team and
    drawing from the landing page. Also, it will be weird not seeing the
    teams on the landing page especially that we dont land on the last
    activity.

--- Android/app/src/main/java/com/example/android/chat/NotificationUtils.kt
+++ Android/app/src/main/java/com/example/android/chat/NotificationUtils.kt
@@ -13,16 +13,11 @@ import com.example.android.R
 var notification_ID = 0
 
 fun NotificationManager.sendNotification(messageBody: String, applicationContext: AppCompatActivity){
-    val contentIntent = Intent(applicationContext, LandingPage::class.java)
-    val contentPendingIntent = PendingIntent.getActivity(applicationContext,
-        notification_ID, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT)
-
     val builder = NotificationCompat.Builder(applicationContext,
         applicationContext.getString(R.string.color_image_notification_id) )
         .setSmallIcon(R.drawable.logo)
         .setContentTitle(applicationContext.getString(R.string.notification_title))
         .setContentText(messageBody)
-        .setContentIntent(contentPendingIntent)
         .setAutoCancel(true)
     notify(notification_ID, builder.build())
     notification_ID++
